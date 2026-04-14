fd05e3e3a8844c79ca64f8b0f4130cd4dca0bfb0
Nurhidayah <nrhidyaa856@gmail.com>
2026-04-14 06:49:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1397283798-9d9a3",
+  "appId": "1:91114263483:web:8e3f05d9d41bfdb0762a9e",
+  "apiKey": "AIzaSyD7IC6egWTvNG4FA9mUjlD2MYBbB5FBKcc",
+  "authDomain": "studio-1397283798-9d9a3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "91114263483"
+};
